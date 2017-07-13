50d1b3b48be18a68f59567dc993ea78926a8d21d
laizamusa <chabi050613@gmail.com>
2017-07-13 13:56:28 +0800

    updated homepage

--- resources/views/home/homepage.blade.php
+++ resources/views/home/homepage.blade.php
@@ -12,5 +12,6 @@
     @include('home.section4')
     @include('home.section5')
     @include('home.section6')
+    @include('home.section7')
 
 @endsection
--- resources/views/home/section3.blade.php
+++ resources/views/home/section3.blade.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ROEL
- * Date: 7/11/2017
- * Time: 3:28 PM
- */
+<div class="row section3 align-items-center">
+    <div class="col">
+        <h1 class="slogan1">PALAYAN: THE CITY OF THE FUTURE</h1>
+        <h6 class="slogan-sub1">The PCBH perfectly reflects the values and character of Palayan City: spacious and green; future-oriented; nimble, efficient, and steadily moving forward. As a partnership between the government and the private sector, it aims to maximize the impact of infrastructure development, an increasingly skilled labor force,
+            and a province set on harnessing long-latent energies to ignite the engines of progress and development.</h6>
+    </div>
+</div>
