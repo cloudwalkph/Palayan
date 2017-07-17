66126bac6c85f1197d02c657f82012e48ab75d3c
laizamusa <chabi050613@gmail.com>
2017-07-17 18:44:07 +0800

    adjust navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-fixed-top navbar-default probootstrap-navbar scrolled">
+        <nav class="navbar navbar-fixed-top navbar-default probootstrap-navbar scrolled navbar-white">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
